8b5fd153ca18a5c95e904783539e8b291e9a8b9d
Todor <todor.ivanov@primeholding.com>
2017-11-10 11:35:41 +0200

    Add courses page

---
+++ src/components/courses/Courses.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Courses extends React.Component {
+
+    render() {
+        return (
+            <div >
+                <h1>Courses</h1>
+            </div>
+        );
+    }
+}
+
+export default Courses;
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { IndexRoute, Route } from "react-router";
 import App from "./components/App";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
+import Courses from "./components/courses/Courses";
 
 
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={Home}/>
         <Route path="about" component={About}/>
+        <Route path="courses" component={Courses}/>
     </Route>
 );
